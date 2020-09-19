91b185db4ef1bd26070ac184fb5d560f73dbcd9f
DenisCo1100100 <kodek_den2001@mail.ru>
2020-09-19 10:14:16 +0300

    Implementing bezier curves

---
+++ Assets/Scripts/BezierLerp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class BezierLerp
+{
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        Vector3 p01 = Vector3.Lerp(p0, p1, t);
+        Vector3 p12 = Vector3.Lerp(p1, p2, t);
+        Vector3 p23 = Vector3.Lerp(p2, p3, t);
+
+        Vector3 p012 = Vector3.Lerp(p01, p12, t);
+        Vector3 p123 = Vector3.Lerp(p12, p23, t);
+
+        Vector3 p0123 = Vector3.Lerp(p012, p123, t);
+
+        return p0123;
+    }
+}
---
+++ Assets/Scripts/BezierTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class BezierTest : MonoBehaviour
+{
+    public Transform p0;
+    public Transform p1;
+    public Transform p2;
+    public Transform p3;
+
+    [Range(0, 1)]
+    public float t;
+
+    void Update()
+    {
+        transform.position = BezierLerp.GetPoint(p0.position, p1.position, p2.position, p3.position, t);
+        transform.rotation = Quaternion.LookRotation(BezierMath.GetFirstDerivative(p0.position, p1.position, p2.position, p3.position, t));
+    }
+
+    //Отрисовка кривой
+    private void OnDrawGizmos()
+    {
+        int sigmentsNumber = 20;
+        Vector3 preveousePoint = p0.position;
+
+        for (int i = 0; i < sigmentsNumber + 1; i++)
+        {
+            float parement = (float)i / sigmentsNumber;
+            Vector3 point = BezierLerp.GetPoint(p0.position, p1.position, p2.position, p3.position, parement);
+            Gizmos.DrawLine(preveousePoint, point);
+            preveousePoint = point;
+        }
+    }
+}
